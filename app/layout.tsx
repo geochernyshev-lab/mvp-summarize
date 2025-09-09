8bb4553788bb624628a722c237cb11b31f12ee8f
geochernyshev-lab <geochernyshev@gmail.com>
2025-09-10 02:49:50 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
 import '../styles/globals.css';
-import Header from '@/components/Header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ru"><body>
-      <div className="container">
-        <Header />
-        {children}
-      </div>
-    </body></html>
+    <html lang="ru">
+      <body>
+        <div className="container">
+          {children}
+        </div>
+      </body>
+    </html>
   );
 }
