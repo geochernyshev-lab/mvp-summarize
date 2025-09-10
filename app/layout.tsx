e13a217a1e42fb8b970750458b0a9166e50b5c2c
geochernyshev-lab <geochernyshev@gmail.com>
2025-09-10 19:56:00 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import '../styles/globals.css';
+import Header from '@/components/Header';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
       <body>
         <div className="container">
+          <Header />
           {children}
         </div>
       </body>
